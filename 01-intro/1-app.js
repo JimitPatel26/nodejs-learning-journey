b289fe67c36fc4bdca2f5a0f3812cde2edb0ee85
Jimit Patel <jimitpatel2606@gmail.com>
2026-04-21 23:28:36 +0530

    Added First Node js learning

---
+++ 01-intro/1-app.js
@@ -0,0 +1,8 @@
+const amount = 9;
+if(amount<10){
+    console.log("Small Number");
+}
+else{
+    console.log("Number is Large");
+}
+console.log("Hey it's my First Node App !!!")
---
+++ 01-intro/README.md
@@ -0,0 +1,7 @@
+# 01 - Introduction to Node.js
+
+This is my first Node.js program.
+
+## What I Learned
+- Running JavaScript using Node.js
+- Basic conditional statements
