d776a0425c8319570b78ac1c620e20c1ba4f6301
Jimit Patel <jimitpatel2606@gmail.com>
2026-04-23 23:19:53 +0530

    read file with utf-8 encoding in sync and async

--- 05-file-system/utf8-read.js
+++ 05-file-system/utf8-read.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 
 // Async UTF-8
-fs.readFile('data.txt', 'utf-8', (err, data) => {
+fs.readFile('data/data.txt', 'utf-8', (err, data) => {
     if (err) throw err;
     console.log("Async UTF-8:");
     console.log(data);
 });
 
 // Sync UTF-8
-const dataSync = fs.readFileSync('data.txt', 'utf-8');
+const dataSync = fs.readFileSync('data/data.txt', 'utf-8');
 
 console.log("Sync UTF-8:");
 console.log(dataSync);
